033e5ccc492e2ad15dbbfb983d0e7837104a3f52
Pietra Kruger Andrade <Pietrapikruger11@gmail.com>
2026-03-16 14:53:19 -0300

    revisão das atividades do dia 12.03
    esqueci de alterar algumas coisas, então salvei de novo

--- 16.03.2026/src/server.js
+++ 16.03.2026/src/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 let frutas = [
-  {id: 1, nome: "Pietra", idade: "16"},
-  {id: 2, nome: "Pedro", idade: "16"},
-  {id: 3, nome: "Edio", idade: "13556"}
+  {id: 1, nome: "Banana"},
+  {id: 2, nome: "Maçã"},
+  {id: 3, nome: "Laranja"}
 ]
 
 app.get('/frutas/:id', (req, res) => {
