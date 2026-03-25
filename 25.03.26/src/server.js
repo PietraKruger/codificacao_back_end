351b6d30f55e1648279b095297a0c0858a9d8009
PIETRA KRUGER ANDRADE <pietra_andrade@estudante.sesisenai.org.br>
2026-03-25 16:59:00 -0300

    atividades de hoje
    25.03.26

--- 25.03.26/src/server.js
+++ 25.03.26/src/server.js
@@ -1,14 +1,14 @@
 import express from 'express'
-import {routeAlunos} from '../src/routes/alunos.routes'
+import route from './routes/filmes.routes.js'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
 })
 
-app.use('/filmes', )
+app.use('/filmes', route)
 app.listen(port, () => {
-  console.log('Example app listening on port ${port}')
+  console.log(`Example app listening on port ${port}`)
 })
 
 //DB > Service > Routes > server.js
